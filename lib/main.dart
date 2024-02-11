f7c1875a10f98b7e0ae31ca27d1de0ec936e71e1
sharif <shorif.ru.019@gmail.com>
2024-02-11 00:37:50 +0000

    Completed issue #3: Set App's Default Color and Global Font
    - Defined required colors.
    - Selected Kameron-Regular font family as global font for text consistency
    - Tested app on various devices to ensure consistent appearance and readability

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    bool darkModeEnabled = false;
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: darkModeEnabled ? darkTheme : lightTheme,
+      darkTheme: darkTheme,
       home: const MyHomePage(),
     );
   }
@@ -24,6 +24,54 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Testing color and font"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+    );
   }
 }
+
+
+// Light Theme
+final ThemeData lightTheme = ThemeData(
+  fontFamily: 'Kameron',
+  // brightness: Brightness.light,
+  // primaryColor: Colors.white,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light,
+    background: Colors.white,
+    onBackground: Colors.black,
+    primary: Color(0xffC70039),
+    onPrimary: Colors.white,
+    secondary: Color(0xff008080),
+    onSecondary: Colors.white,
+    tertiary: Color(0xff757575),
+    error: Colors.red,
+    onError: Colors.white,
+    surface: Colors.white54,
+    onSurface: Colors.black,
+  ),
+);
+
+// Dark Theme
+final ThemeData darkTheme = ThemeData(
+  fontFamily: 'Kameron',
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light,
+    background: Colors.black,
+    onBackground: Colors.white,
+    primary: Color(0xffC70039),
+    onPrimary: Colors.white,
+    secondary: Color(0xff008080),
+    onSecondary: Colors.white,
+    tertiary: Color(0xff757575),
+    error: Colors.red,
+    onError: Colors.white,
+    surface: Colors.white54,
+    onSurface: Colors.black,
+    shadow: Color(0xff757575),
+  ),
+);
+
